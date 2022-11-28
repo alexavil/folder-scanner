473b690f64239778cffede41bc18e7370f2c3060
Inspector Soviet <www.alex.avil@mail.ru>
2022-11-28 16:18:35 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scan files in a folder
+
+This action will scan all files in a specified folder and commit a .json file containing all file names.
---
+++ index.js
@@ -0,0 +1,45 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs-extra");
+const { exec } = require("child_process");
+const { stderr } = require("process");
+
+try {
+  const email = core.getInput("email") || process.argv[2];
+  const username = core.getInput("username") || process.argv[3];
+  const folder = core.getInput("folder") || process.argv[4];
+  console.log(`Scanning ${folder}...`);
+  function scan(folder) {
+    const files = fs.readdirSync(folder);
+    console.log(files);
+    let filelist = {
+      files: files,
+    };
+    fs.writeJsonSync(`${folder}files.json`, filelist);
+    files.forEach((file) => {
+      console.log(folder + "/" + file);
+      console.log(fs.statSync(folder + "/" + file).isDirectory());
+      if (fs.statSync(folder + "/" + file).isDirectory()) {
+        scan(folder + "/" + file);
+      }
+    });
+  }
+  scan(folder);
+  //Commit back to Github.
+  exec(`git config user.email ${email}`);
+  exec(`git config user.name ${username}`);
+  exec(
+    `git add --all && git commit -m "Add List of Files"`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(stderr);
+        console.log(stdout);
+        exec("git push");
+      }
+    }
+  );
+} catch (error) {
+  core.setFailed(error.message);
+}
