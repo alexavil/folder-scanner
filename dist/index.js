473131403768c8181dabe14c3c19e98dae379f72
alexavil <alexavil007@gmail.com>
2026-02-03 10:46:20 +0000

    remove ignored folders

--- dist/index.js
+++ dist/index.js
@@ -33725,22 +33725,10 @@ function getIDToken(aud) {
 const index_fs = __nccwpck_require__(2136);
 const { exec: index_exec } = __nccwpck_require__(5317);
 
-const default_ignored_folders = ["node_modules", ".git", ".github", ".vscode"];
-
 const email = getInput("email");
 const username = getInput("username");
 const folder = getInput("folder");
-const custom_ignored_folders = getInput("ignored_folders");
-const include_ignored_folders = getInput("include_ignored_folders");
 const commit_message = getInput("commit_message");
-
-if (include_ignored_folders !== "true" && include_ignored_folders !== "false") {
-  throw new Error("include_ignored_folders must be true or false");
-}
-let ignored_folders = default_ignored_folders;
-if (custom_ignored_folders) {
-  ignored_folders = ignored_folders.concat(custom_ignored_folders.split(", "));
-}
 
 async function setup() {
   try {
@@ -33763,20 +33751,13 @@ async function scan(folder) {
       files = files.filter((file) => file !== "files.json");
     }
 
-    if (include_ignored_folders === "false") {
-      files = files.filter((file) => !ignored_folders.includes(file));
-    }
-
     let filelist = {
       files: files,
     };
 
     index_fs.writeJsonSync(`${folder}/files.json`, filelist);
     files.forEach((file) => {
-      if (
-        index_fs.statSync(folder + "/" + file).isDirectory() &&
-        !ignored_folders.includes(file)
-      ) {
+      if (index_fs.statSync(folder + "/" + file).isDirectory()) {
         scan(folder + "/" + file);
       }
     });
@@ -33787,38 +33768,11 @@ async function scan(folder) {
 }
 
 function createCommit(oldFiles, files) {
-  console.log(oldFiles, files);
-  if (oldFiles === files)
-    return console.log("No changes to the file structure!");
+  if (oldFiles === files) return console.log("No changes detected!");
   else {
     try {
       console.log("Creating a commit...");
-      index_exec(
-        `git add --all && git commit -m "${commit_message}"`,
-        (err, stdout, stderr) => {
-          if (err) {
-            console.log(stderr);
-            console.log(stdout);
-            console.log(err);
-            setFailed(err.message);
-          } else {
-            console.log(stderr);
-            console.log(stdout);
-            console.log("Pushing the commit...");
-            index_exec("git push", (err, stdout, stderr) => {
-              if (err) {
-                console.log(stderr);
-                console.log(stdout);
-                console.log(err);
-                setFailed(err.message);
-              } else {
-                console.log(stderr);
-                console.log(stdout);
-              }
-            });
-          }
-        },
-      );
+      index_exec(`git add --all && git commit -m "${commit_message}" && git push`);
     } catch (error) {
       setFailed(error.message);
     }
--- index.js
+++ index.js
@@ -2,22 +2,10 @@ import * as core from "@actions/core";
 const fs = require("fs-extra");
 const { exec } = require("child_process");
 
-const default_ignored_folders = ["node_modules", ".git", ".github", ".vscode"];
-
 const email = core.getInput("email");
 const username = core.getInput("username");
 const folder = core.getInput("folder");
-const custom_ignored_folders = core.getInput("ignored_folders");
-const include_ignored_folders = core.getInput("include_ignored_folders");
 const commit_message = core.getInput("commit_message");
-
-if (include_ignored_folders !== "true" && include_ignored_folders !== "false") {
-  throw new Error("include_ignored_folders must be true or false");
-}
-let ignored_folders = default_ignored_folders;
-if (custom_ignored_folders) {
-  ignored_folders = ignored_folders.concat(custom_ignored_folders.split(", "));
-}
 
 async function setup() {
   try {
@@ -40,20 +28,13 @@ async function scan(folder) {
       files = files.filter((file) => file !== "files.json");
     }
 
-    if (include_ignored_folders === "false") {
-      files = files.filter((file) => !ignored_folders.includes(file));
-    }
-
     let filelist = {
       files: files,
     };
 
     fs.writeJsonSync(`${folder}/files.json`, filelist);
     files.forEach((file) => {
-      if (
-        fs.statSync(folder + "/" + file).isDirectory() &&
-        !ignored_folders.includes(file)
-      ) {
+      if (fs.statSync(folder + "/" + file).isDirectory()) {
         scan(folder + "/" + file);
       }
     });
@@ -64,38 +45,11 @@ async function scan(folder) {
 }
 
 function createCommit(oldFiles, files) {
-  console.log(oldFiles, files);
-  if (oldFiles === files)
-    return console.log("No changes to the file structure!");
+  if (oldFiles === files) return console.log("No changes detected!");
   else {
     try {
       console.log("Creating a commit...");
-      exec(
-        `git add --all && git commit -m "${commit_message}"`,
-        (err, stdout, stderr) => {
-          if (err) {
-            console.log(stderr);
-            console.log(stdout);
-            console.log(err);
-            core.setFailed(err.message);
-          } else {
-            console.log(stderr);
-            console.log(stdout);
-            console.log("Pushing the commit...");
-            exec("git push", (err, stdout, stderr) => {
-              if (err) {
-                console.log(stderr);
-                console.log(stdout);
-                console.log(err);
-                core.setFailed(err.message);
-              } else {
-                console.log(stderr);
-                console.log(stdout);
-              }
-            });
-          }
-        },
-      );
+      exec(`git add --all && git commit -m "${commit_message}" && git push`);
     } catch (error) {
       core.setFailed(error.message);
     }
