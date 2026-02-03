557e7716236b967d4eefa3b213f5c46388b6b332
alexavil <alexavil007@gmail.com>
2026-02-03 10:33:31 +0000

    cleanup test

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const folder = core.getInput("folder");
 const custom_ignored_folders = core.getInput("ignored_folders");
 const include_ignored_folders = core.getInput("include_ignored_folders");
 const commit_message = core.getInput("commit_message");
+
 if (include_ignored_folders !== "true" && include_ignored_folders !== "false") {
   throw new Error("include_ignored_folders must be true or false");
 }
@@ -18,71 +19,87 @@ if (custom_ignored_folders) {
   ignored_folders = ignored_folders.concat(custom_ignored_folders.split(", "));
 }
 
-console.log("Setting up git...");
-exec(`git config user.email ${email}`);
-exec(`git config user.name ${username}`);
-console.log(`Scanning ${folder}...`);
-scan(folder).then(diff => createCommit(diff.oldFiles, diff.files));
+async function setup() {
+  try {
+    console.log("Setting up git...");
+    await exec(`git config user.email ${email}`);
+    await exec(`git config user.name ${username}`);
+    let diff = await scan(folder);
+    createCommit(diff.oldFiles, diff.files);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
 
 async function scan(folder) {
-  let files = fs.readdirSync(folder);
-  let oldFiles = undefined;
-  if (files.includes("files.json")) {
-    oldFiles = fs.readJSONSync(`${folder}/files.json`);
-    files.splice(files.indexOf("files.json"), 1);
-  } else {
-    oldFiles = [];
-  }
-  if (include_ignored_folders === "false") {
-    let filter = files.filter((file) => ignored_folders.includes(file));
-    filter.forEach((file) => files.splice(files.indexOf(file), 1));
-  }
-  let filelist = {
-    files: files,
-  };
-  fs.writeJsonSync(`${folder}/files.json`, filelist);
-  files.forEach((file) => {
-    if (
-      fs.statSync(folder + "/" + file).isDirectory() &&
-      !ignored_folders.includes(file)
-    ) {
-      scan(folder + "/" + file);
+  try {
+    let files = fs.readdirSync(folder);
+    let oldFiles = [];
+    if (files.includes("files.json")) {
+      oldFiles = fs.readJSONSync(`${folder}/files.json`);
+      files = files.filter((file) => file !== "files.json");
     }
-  });
-  return { oldFiles, files };
+
+    if (include_ignored_folders === "false") {
+      files = files.filter((file) => !ignored_folders.includes(file));
+    }
+
+    let filelist = {
+      files: files,
+    };
+
+    fs.writeJsonSync(`${folder}/files.json`, filelist);
+    files.forEach((file) => {
+      if (
+        fs.statSync(folder + "/" + file).isDirectory() &&
+        !ignored_folders.includes(file)
+      ) {
+        scan(folder + "/" + file);
+      }
+    });
+    return { oldFiles, files };
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 function createCommit(oldFiles, files) {
-  console.log(oldFiles, files)
+  console.log(oldFiles, files);
   if (oldFiles === files)
     return console.log("No changes to the file structure!");
   else {
-    console.log("Creating a commit...");
-    exec(
-      `git add --all && git commit -m "${commit_message}"`,
-      (err, stdout, stderr) => {
-        if (err) {
-          console.log(stderr);
-          console.log(stdout);
-          console.log(err);
-          core.setFailed(err.message);
-        } else {
-          console.log(stderr);
-          console.log(stdout);
-          console.log("Pushing the commit...");
-          exec("git push", (err, stdout, stderr) => {
-            if (err) {
-              console.log(stderr);
-              console.log(stdout);
-              console.log(err);
-              core.setFailed(err.message);
-            } else {
-              console.log(stderr);
-              console.log(stdout);
-            }
-          });
-        }
-      },
-    );
+    try {
+      console.log("Creating a commit...");
+      exec(
+        `git add --all && git commit -m "${commit_message}"`,
+        (err, stdout, stderr) => {
+          if (err) {
+            console.log(stderr);
+            console.log(stdout);
+            console.log(err);
+            core.setFailed(err.message);
+          } else {
+            console.log(stderr);
+            console.log(stdout);
+            console.log("Pushing the commit...");
+            exec("git push", (err, stdout, stderr) => {
+              if (err) {
+                console.log(stderr);
+                console.log(stdout);
+                console.log(err);
+                core.setFailed(err.message);
+              } else {
+                console.log(stderr);
+                console.log(stdout);
+              }
+            });
+          }
+        },
+      );
+    } catch (error) {
+      core.setFailed(error.message);
+    }
   }
 }
+
+setup();
